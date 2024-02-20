b4d0d3e7ca71eb567577f95290abc81bdfe44cae
chrxmehearts <sengvihss@gmail.com>
2024-02-20 23:09:12 +0100

    New adding

---
+++ topchik/fine.java
@@ -0,0 +1,9 @@
+package topchik;
+
+public class fine {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hi" + i);
+        }
+    }
+}
